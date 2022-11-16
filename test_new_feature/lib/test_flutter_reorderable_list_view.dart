d713ee261c441a1bc02cde0e555059265c960a21
Nhin <nhinlechi@gmail.com>
2022-11-16 16:14:49 +0700

    Solved problem with SliverReorderableList

--- test_new_feature/lib/test_flutter_reorderable_list_view.dart
+++ test_new_feature/lib/test_flutter_reorderable_list_view.dart
@@ -17,62 +17,73 @@ class _TestFlutterReorderableListViewState
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
+      body: CustomScrollView(
         controller: controller,
-        child: Column(
-          children: [
-            _buildReorderableList(),
-          ],
-        ),
+        slivers: [
+          SliverToBoxAdapter(
+            child: Container(
+              padding: const EdgeInsets.all(16),
+              child: const Text(
+                'Ordering Question',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              padding: const EdgeInsets.all(16),
+              child: const Text(
+                '''Why do we use it?
+It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).''',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ),
+          SliverReorderableList(
+            itemBuilder: (context, index) {
+              return ReorderableDragStartListener(
+                index: index,
+                key: Key('Answer item: $index'),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  child: ChoiceItem(
+                    index: index,
+                    text: 'Answer item: ${_items[index]}',
+                  ),
+                ),
+              );
+            },
+            itemCount: _items.length,
+            onReorder: _onReorder,
+            proxyDecorator: _proxyDecorator,
+          )
+        ],
       ),
     );
   }
 
-  _buildReorderableList() {
-    return ReorderableListView(
-      scrollController: controller,
-      header: const Text(
-        'ReorderableListView',
-        style: TextStyle(
-          fontSize: 20,
-        ),
+  _onReorder(int oldIndex, int newIndex) {
+    if (newIndex > oldIndex) {
+      newIndex--;
+    }
+    setState(() {
+      final int item = _items.removeAt(oldIndex);
+      _items.insert(newIndex, item);
+    });
+  }
+
+  Widget _proxyDecorator(Widget child, int index, Animation<double> animation) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      child: ChoiceItem(
+        index: index,
+        text: 'Answer item: ${_items[index]}',
+        active: true,
       ),
-      padding: const EdgeInsets.all(32),
-      children: <Widget>[
-        for (int index = 0; index < _items.length; index++)
-          ReorderableDragStartListener(
-            index: index,
-            key: Key('Answer item: $index'),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8),
-              child: ChoiceItem(
-                index: index,
-                text: 'Answer item: ${_items[index]}',
-              ),
-            ),
-          ),
-      ],
-      onReorder: (int oldIndex, int newIndex) {
-        if (newIndex > oldIndex) {
-          newIndex--;
-        }
-        setState(() {
-          final int item = _items.removeAt(oldIndex);
-          _items.insert(newIndex, item);
-        });
-      },
-      buildDefaultDragHandles: false,
-      shrinkWrap: true,
-      proxyDecorator: (child, index, animation) {
-        return Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8),
-          child: ChoiceItem(
-            index: index,
-            text: 'Answer item: ${_items[index]}',
-            active: true,
-          ),
-        );
-      },
     );
   }
 }
